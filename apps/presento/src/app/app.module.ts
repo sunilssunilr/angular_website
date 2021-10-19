c883bacf74e4c9a39c68c6a5d9d2dcc04e00e276
SR <sunilssunilr@gmail.com>
2021-10-19 16:13:11 +0530

    changes

--- apps/presento/src/app/app.module.ts
+++ apps/presento/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './footer/footer.component';``
+// import { BoxiconsModule } from 'boxicons';
+import { SwiperModule } from 'swiper/angular';
+// import GLightbox from 'glightbox';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    SwiperModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
